608accbbb96aff32c432b9a15d4faa281a2f2e6b
Ubuntu <ubuntu@test2.novalocal>
2023-01-17 10:12:10 +0000

    test

--- website/app/routes.py
+++ website/app/routes.py
@@ -1,9 +1,36 @@
 from app import app
+from app.sendCircuit import sendCircuit
 
 from flask import (
     render_template,
 )
 
+import sys
+import socket
+import os
+import time
+
+HOST = "124.188.226.203"  # The server's hostname or IP address
+PORT = 65432  # The port used by the server
+
+BUFFER_SIZE = 4096
+
 @app.route("/")
-def hello_world():
+def index():
+    return render_template('index.html')
+
+@app.route("/sendCircuit")
+def sendCircuit():
+    filename = 'cif.qasm'
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+        s.sendall(f"{filename}".encode())
+        print('filename sent')
+        with open(filename, "r") as f:
+            data = f.read()
+            f.close()
+            s.sendall(data.encode())
+            print("circuit sent")
+            response = s.recv(BUFFER_SIZE).decode()
+            print(response)
     return render_template('index.html')
--- website/app/sendCircuit.py
+++ website/app/sendCircuit.py
@@ -3,20 +3,21 @@ import socket
 import os
 import time
 
-HOST = "10.0.0.133"  # The server's hostname or IP address
+HOST = "124.188.226.203"  # The server's hostname or IP address
 PORT = 65432  # The port used by the server
 
 BUFFER_SIZE = 4096
 
-def sendCircuit(filename):
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((HOST, PORT))
-        s.sendall(f"{filename}".encode())
-        print('filename sent')
-        with open(filename, "r") as f:
-            data = f.read()
-            f.close()
-            s.sendall(data.encode())
-            print("circuit sent")
-            response = s.recv(BUFFER_SIZE).decode()
-            print(response)
+filename = sys.argv[1]
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    s.sendall(f"{filename}".encode())
+    print('filename sent')
+    with open(filename, "r") as f:
+        data = f.read()
+        f.close()
+        s.sendall(data.encode())
+        print("circuit sent")
+        response = s.recv(BUFFER_SIZE).decode()
+        print(response)

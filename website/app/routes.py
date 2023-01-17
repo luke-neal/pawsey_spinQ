4a6e210770cc59c3e9e02f470a053a12e9399694
Ubuntu <ubuntu@test2.novalocal>
2023-01-17 16:48:35 +0000

    website changes

--- website/app/routes.py
+++ website/app/routes.py
@@ -1,8 +1,8 @@
 from app import app
-from app.sendCircuit import sendCircuit
 
 from flask import (
     render_template,
+    request
 )
 
 import sys
@@ -15,22 +15,40 @@ PORT = 65432  # The port used by the server
 
 BUFFER_SIZE = 4096
 
+def sendCircuit(file):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+        s.sendall(f"{file.filename}".encode())
+        print('filename sent')
+        s.sendall(file.read())
+        print("circuit sent")
+        response = s.recv(BUFFER_SIZE).decode()
+        return response
+
+ALLOWED_EXTENSIONS = {'qasm'}
+
 @app.route("/")
 def index():
     return render_template('index.html')
 
-@app.route("/sendCircuit")
-def sendCircuit():
-    filename = 'cif.qasm'
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((HOST, PORT))
-        s.sendall(f"{filename}".encode())
-        print('filename sent')
-        with open(filename, "r") as f:
-            data = f.read()
-            f.close()
-            s.sendall(data.encode())
-            print("circuit sent")
-            response = s.recv(BUFFER_SIZE).decode()
-            print(response)
-    return render_template('index.html')
+@app.route("/submitcircuit")
+def submit_circuit():
+    return render_template('submit_circuit.html')
+
+@app.route('/circuitresult', methods=['POST', 'GET'])
+def circuit_result():
+    if 'file' not in request.files:
+        return 'No file uploaded'
+    file = request.files['file']
+    if file.filename == '':
+        return 'No file selected'
+    if file and allowed_file(file.filename):
+        result = sendCircuit(file)
+        return render_template('circuit_result.html', result=result)
+    return 'Invalid file type'
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+if __name__ == '__main__':
+    app.run(debug=True)
